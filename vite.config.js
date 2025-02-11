1fc1807afa723fdd039fcc8a1e52cc925e5d2c02
Joshua Rose <joshuarose@gmx.com>
2025-02-11 17:31:33 +1100

    feat(vite): added vite testing framework

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import path from 'path';
+import { sveltekit } from "@sveltejs/kit/vite";
+import path from "path";
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	resolve: {
-		alias: {
-			$routes: path.resolve('./src/routes')
-		}
-	}
+  plugins: [sveltekit()],
+  resolve: {
+    alias: {
+      $routes: path.resolve("./src/routes"),
+    },
+  },
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+    environment: "jsdom",
+  },
 };
 
 export default config;
